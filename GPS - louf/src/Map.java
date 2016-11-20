a9a825753c659341f47c71e0e875936c2e0f4039
Freeman Lou <freemanlou2430@yahoo.com>
2016-11-19 23:36:11 -0500

    Added distance calculator function

--- GPS - louf/src/Map.java
+++ GPS - louf/src/Map.java
@@ -1,13 +1,12 @@
 import java.util.*;
 /**
- * 
  * Object representing a map and also
  * an undirected graph
  * 
  * @author Freeman
  */
 
-public class Map implements Graph{
+public class Map implements Graph {
     private HashMap<String, GraphNode> nodes;	// Id and node
     private HashMap<String, Way> ways;		// Id and way
     private HashMap<String, Relation> relations; // Id and relation
@@ -136,5 +135,47 @@ public class Map implements Graph{
     @Override
     public ArrayList<GraphEdge> getRoute(GraphNode a, GraphNode b) {
 	return null;
+    }
+    
+    /**
+     * Get closest node
+     * 
+     * @param latitude
+     * @param longitude
+     * @return GPSNode that is closest to specified point
+     */
+    public GPSNode getClosestNode(double lat, double lon) {
+	GPSNode closest = null;
+	double min = Double.MAX_VALUE;
+	
+	
+	
+	return null;
+    }
+    
+    /**
+     * Calculates distance between two coordinate sets 
+     * 
+     * @param latitude1
+     * @param longitude1
+     * @param latitude2
+     * @param longitude2
+     * @return double distance
+     */
+    public double calcDistance(double lat1, double lon1, double lat2, double lon2) {
+	//Calculate great-circle distance
+	double radius = 6371000;	//radius of earth in meters
+	double deltaLat = Math.toRadians(lat2 - lat1);
+	double deltaLong = Math.toRadians(lon2 - lon1);
+	
+	lat1 = Math.toRadians(lat1);
+	lat2 = Math.toRadians(lat2);
+		
+	double a = Math.sin(deltaLat / 2.0) * Math.sin(deltaLat / 2.0) +
+		Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLong / 2.0) *
+		Math.sin(deltaLong / 2.0);
+	double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0 - a));
+	
+	return radius * c;
     }
 }
